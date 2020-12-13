2833555fb4b4dec52a6cd78aa5bda2bf02436c00
Jinhyeok Kil <roadjh0201@gmail.com>
2020-12-13 16:05:47 +0900

    hide secret information

--- index.js
+++ index.js
@@ -2,22 +2,45 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const config = require("./config/key");
+
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://zknock:cert@0188@boilerplate.xyvpy.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  // 회원가입할 때 필요한 정보들을 client에서 가져오면
+  // DB에 입력
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    //mongoDB method
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
