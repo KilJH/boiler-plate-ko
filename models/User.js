f247de827aeb094536d9086cba55467753613085
Jinhyeok Kil <roadjh0201@gmail.com>
2020-12-14 12:30:55 +0900

    encrypt password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -29,6 +31,49 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+/*
+//저장하기 전에 next라는
+userSchema.pre("save", (next) => {
+  const user = this;
+
+  console.log("hashing...");
+
+
+  //비밀번호가 변경될 때에만
+  if (user.isModified("password")) {
+    //비밀번호 암호화
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+        user.password = hash;
+        console.log(user.password);
+        next();
+      });
+    });
+  }
+});*/
+
+//under ES6
+userSchema.pre("save", function (next) {
+  const user = this;
+  console.log("Encrypt...");
+
+  //비밀번호가 변경될 때에만
+  if (user.isModified("password")) {
+    //비밀번호 암호화
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+        user.password = hash;
+        console.log(user.password);
+        next();
+      });
+    });
+  }
 });
 
 const User = mongoose.model("User", userSchema);
