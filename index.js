8f77f5ea3ede906b9d132894e33d89a83998cddf
Jinhyeok Kil <roadjh0201@gmail.com>
2020-12-14 13:08:55 +0900

    login

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 5000;
 
 const config = require("./config/key");
 
+const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const { User } = require("./models/User");
 
@@ -12,6 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //application/json
 app.use(bodyParser.json());
+
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -28,6 +31,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// 회원가입
 app.post("/register", (req, res) => {
   // 회원가입할 때 필요한 정보들을 client에서 가져오면
   // DB에 입력
@@ -39,6 +43,39 @@ app.post("/register", (req, res) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+
+//  로그인
+app.post("/login", (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "해당하는 이메일이 없습니다.",
+      });
+    }
+    // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는지 확인한다.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다",
+        });
+      }
+
+      // 비밀번호까지 맞다면 토큰을 생성한다.
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // token을 쿠키에 저장한다.
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
     });
   });
 });
