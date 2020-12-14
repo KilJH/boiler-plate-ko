51ebd2ff049b4ecd505b99a379601253faa143fb
Jinhyeok Kil <roadjh0201@gmail.com>
2020-12-14 13:38:33 +0900

    authentication

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const config = require("./config/key");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const { User } = require("./models/User");
+const { auth } = require("./middleware/auth");
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,7 +33,7 @@ app.get("/", (req, res) => {
 });
 
 // 회원가입
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   // 회원가입할 때 필요한 정보들을 client에서 가져오면
   // DB에 입력
 
@@ -48,7 +49,7 @@ app.post("/register", (req, res) => {
 });
 
 //  로그인
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
   // 요청된 이메일을 데이터베이스에서 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -77,6 +78,21 @@ app.post("/login", (req, res) => {
           .json({ loginSuccess: true, userId: user._id });
       });
     });
+  });
+});
+
+// 인증
+app.get("/api/users/auth", auth, (req, res) => {
+  // 여기까지 미들웨어가 통과해 왔다는 얘기는 Authentication이 true라는 말
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
   });
 });
 
