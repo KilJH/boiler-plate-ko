cd73ccc378989dd3e035b3c1f007cbbf7248c79f
Jinhyeok Kil <roadjh0201@gmail.com>
2020-12-22 13:16:27 +0900

    create components

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'antd/dist/antd.css';
+import { createStoreHook, Provider } from 'react-redux';
+import { applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+// 객체 외에 promise 같은 것도 스토어에 저장하기 위함 - 미들웨어 이용
+const createStoreWithMiddleware = applyMiddleware(
+	promiseMiddleware,
+	ReduxThunk
+)(createStore);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Provider
+		store={createStoreWithMiddleware(
+			Reducer,
+			window.__REDUX_DEVTOOLS_EXTENSION__ &&
+				window.__REDUX_DEVTOOLS_EXTENSION__()
+		)}
+	>
+		<App />
+	</Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 5000;
 
-const config = require("./config/key");
+const config = require('./server/config/key');
 
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const { User } = require("./models/User");
-const { auth } = require("./middleware/auth");
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const { User } = require('./server/models/User');
+const { auth } = require('./server/middleware/auth');
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,95 +17,99 @@ app.use(bodyParser.json());
 
 app.use(cookieParser());
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 mongoose
-  .connect(config.mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log("MongoDB connected..."))
-  .catch((err) => console.log(err));
+	.connect(config.mongoURI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	})
+	.then(() => console.log('MongoDB connected...'))
+	.catch((err) => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.get('/api/hello', (req, res) => {
+	res.send('Hello!');
 });
 
 // 회원가입
-app.post("/api/users/register", (req, res) => {
-  // 회원가입할 때 필요한 정보들을 client에서 가져오면
-  // DB에 입력
+app.post('/api/users/register', (req, res) => {
+	// 회원가입할 때 필요한 정보들을 client에서 가져오면
+	// DB에 입력
 
-  const user = new User(req.body);
+	const user = new User(req.body);
 
-  user.save((err, userInfo) => {
-    //mongoDB method
-    if (err) return res.json({ success: false, err });
-    return res.status(200).json({
-      success: true,
-    });
-  });
+	user.save((err, userInfo) => {
+		//mongoDB method
+		if (err) return res.json({ success: false, err });
+		return res.status(200).json({
+			success: true,
+		});
+	});
 });
 
 //  로그인
-app.post("/api/users/login", (req, res) => {
-  // 요청된 이메일을 데이터베이스에서 찾는다.
-  User.findOne({ email: req.body.email }, (err, user) => {
-    if (!user) {
-      return res.json({
-        loginSuccess: false,
-        message: "해당하는 이메일이 없습니다.",
-      });
-    }
-    // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는지 확인한다.
-    user.comparePassword(req.body.password, (err, isMatch) => {
-      if (!isMatch) {
-        return res.json({
-          loginSuccess: false,
-          message: "비밀번호가 틀렸습니다",
-        });
-      }
+app.post('/api/users/login', (req, res) => {
+	// 요청된 이메일을 데이터베이스에서 찾는다.
+	User.findOne({ email: req.body.email }, (err, user) => {
+		if (!user) {
+			return res.json({
+				loginSuccess: false,
+				message: '해당하는 이메일이 없습니다.',
+			});
+		}
+		// 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는지 확인한다.
+		user.comparePassword(req.body.password, (err, isMatch) => {
+			if (!isMatch) {
+				return res.json({
+					loginSuccess: false,
+					message: '비밀번호가 틀렸습니다',
+				});
+			}
 
-      // 비밀번호까지 맞다면 토큰을 생성한다.
-      user.generateToken((err, user) => {
-        if (err) return res.status(400).send(err);
+			// 비밀번호까지 맞다면 토큰을 생성한다.
+			user.generateToken((err, user) => {
+				if (err) return res.status(400).send(err);
 
-        // token을 쿠키에 저장한다.
-        res
-          .cookie("x_auth", user.token)
-          .status(200)
-          .json({ loginSuccess: true, userId: user._id });
-      });
-    });
-  });
+				// token을 쿠키에 저장한다.
+				res
+					.cookie('x_auth', user.token)
+					.status(200)
+					.json({ loginSuccess: true, userId: user._id });
+			});
+		});
+	});
 });
 
 // 인증
-app.get("/api/users/auth", auth, (req, res) => {
-  // 여기까지 미들웨어가 통과해 왔다는 얘기는 Authentication이 true라는 말
-  res.status(200).json({
-    _id: req.user._id,
-    isAdmin: req.user.role === 0 ? false : true,
-    isAuth: true,
-    email: req.user.email,
-    name: req.user.name,
-    lastname: req.user.lastname,
-    role: req.user.role,
-    image: req.user.image,
-  });
+app.get('/api/users/auth', auth, (req, res) => {
+	// 여기까지 미들웨어가 통과해 왔다는 얘기는 Authentication이 true라는 말
+	res.status(200).json({
+		_id: req.user._id,
+		isAdmin: req.user.role === 0 ? false : true,
+		isAuth: true,
+		email: req.user.email,
+		name: req.user.name,
+		lastname: req.user.lastname,
+		role: req.user.role,
+		image: req.user.image,
+	});
 });
 
-app.get("/api/users/logout", auth, (req, res) => {
-  console.log("req.user", req.user);
-  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
-    if (err) return res.json({ success: false, err });
-    return res.status(200).send({
-      success: true,
-    });
-  });
+app.get('/api/users/logout', auth, (req, res) => {
+	console.log('req.user', req.user);
+	User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, (err, user) => {
+		if (err) return res.json({ success: false, err });
+		return res.status(200).send({
+			success: true,
+		});
+	});
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+	console.log(`Example app listening at http://localhost:${port}`);
 });
